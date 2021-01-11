3f0bf1c2c876755de3147d8b7ab5e061aee00619
Kate Maltseva <kotoyama@list.ru>
2021-01-11 06:18:28 +0500

    init router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, GlobalStyles } from 'styles'
 
 import App from './app'
 
+const history = createBrowserHistory()
+
 render(
-  <ThemeProvider theme={lightTheme}>
-    <GlobalStyles />
-    <App />
-  </ThemeProvider>,
+  <Router history={history}>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </Router>,
   document.getElementById('root'),
 )
