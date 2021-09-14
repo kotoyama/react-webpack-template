1787596d2427fc7e8b2a654fd67d24125edff127
kotoyama <kotoyama@list.ru>
2021-09-15 00:34:19 +0500

    feat(logger): attach logger via redux devtools

--- src/root.ts
+++ src/root.ts
@@ -1,3 +1,13 @@
 import { createDomain } from 'effector'
 
 export const root = createDomain('root')
+
+if (process.env.NODE_ENV === 'development') {
+  import('effector-logger/attach').then(({ attachLogger }) => {
+    attachLogger(root, {
+      reduxDevtools: 'enabled',
+      inspector: 'disabled',
+      console: 'disabled',
+    })
+  })
+}
