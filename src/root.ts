d254ec1d3eb43ae9bb1c12f7de95059280b8849e
kotoyama <kotoyama@list.ru>
2021-09-15 19:28:27 +0500

    chore: update deps & init lib folder

--- src/root.ts
+++ src/root.ts
@@ -1,8 +1,9 @@
 import { createDomain } from 'effector'
+import { isBrowser } from 'lib/is-browser'
 
 export const root = createDomain('root')
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' && isBrowser) {
   import('effector-logger/attach').then(({ attachLogger }) => {
     attachLogger(root, {
       reduxDevtools: 'enabled',
